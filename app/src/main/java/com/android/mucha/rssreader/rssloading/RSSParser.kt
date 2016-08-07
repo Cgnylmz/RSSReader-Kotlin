0d4a8c98a0167b5f670605258dcb05510d87d781
Patrik Mucha <pat.mucha@gmail.com>
2016-08-07 16:14:58 +0200

    * Throw RSSParserException in case of RSSParser errors.

--- app/src/main/java/com/android/mucha/rssreader/rssloading/RSSParser.kt
+++ app/src/main/java/com/android/mucha/rssreader/rssloading/RSSParser.kt
@@ -1,8 +1,12 @@
 package com.android.mucha.rssreader.rssloading
 
 import android.support.annotation.WorkerThread
+import com.android.mucha.rssreader.rssloading.exceptions.RSSParserException
 import org.xml.sax.InputSource
+import org.xml.sax.SAXException
+import java.io.IOException
 import java.io.InputStream
+import javax.xml.parsers.ParserConfigurationException
 import javax.xml.parsers.SAXParserFactory
 
 /**
@@ -16,19 +20,30 @@ class RSSParser {
      * Parses the given input stream.
      *
      * The method call is blocking until the parsing is finished.
+     *
+     * @return The [RSSFeed] instance. Never null.
+     * @throws RSSParserException if the parsing fails.
      */
     @WorkerThread
+    @Throws(RSSParserException::class)
     fun parseRSS(inputStream: InputStream): RSSFeed {
-        // TODO: catch the exceptions - throw our own RSSParserException
-        val factory = SAXParserFactory.newInstance()
-        val parser = factory.newSAXParser()
-        val handler = RSSXmlHandler();
-        val xmlReader = parser.xmlReader
+        try {
+            val factory = SAXParserFactory.newInstance()
+            val parser = factory.newSAXParser()
+            val handler = RSSXmlHandler();
+            val xmlReader = parser.xmlReader
 
-        val source = InputSource(inputStream)
-        xmlReader.contentHandler = handler;
-        xmlReader.parse(source);
+            val source = InputSource(inputStream)
+            xmlReader.contentHandler = handler;
+            xmlReader.parse(source);
 
-        return handler.rssFeed
+            return handler.rssFeed
+        } catch (e: ParserConfigurationException) {
+            throw RSSParserException("Wrong parser configuration.", e)
+        } catch (e: SAXException) {
+            throw RSSParserException("SAX error occurred.", e)
+        } catch (e: IOException) {
+            throw RSSParserException("IOException during parsing input.", e)
+        }
     }
 }
