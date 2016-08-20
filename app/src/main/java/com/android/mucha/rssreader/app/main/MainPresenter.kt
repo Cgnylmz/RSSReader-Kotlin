e5d772458a2556ed5165054b3039f793ffa92e06
Patrik Mucha <pat.mucha@gmail.com>
2016-08-20 19:35:36 +0200

    * Cache loaded Feed items in Main Presenter.
    Main Presenter holds the loaded Feed result and when config changes (e.g. orientation) then the cached results are shown on view bind.

--- app/src/main/java/com/android/mucha/rssreader/app/main/MainPresenter.kt
+++ app/src/main/java/com/android/mucha/rssreader/app/main/MainPresenter.kt
@@ -2,6 +2,7 @@ package com.android.mucha.rssreader.app.main
 
 import com.android.mucha.rssreader.mvp.BasePresenter
 import com.android.mucha.rssreader.rssloading.RSSFeed
+import com.android.mucha.rssreader.rssloading.RSSFeedItem
 import com.android.mucha.rssreader.rssloading.RSSLoader
 import rx.Single
 import rx.SingleSubscriber
@@ -16,27 +17,48 @@ import rx.schedulers.Schedulers
 class MainPresenter : BasePresenter<MainView>() {
 
     /**
+     * Cache for Feed items.
+     */
+    private var mFeedItems: List<RSSFeedItem>? = null
+
+    override fun bindView(view: MainView) {
+        super.bindView(view)
+
+        // Show cached Feed items if they are available - e.g. after rotation
+        if (mFeedItems != null) {
+            view.showRSSFeed(mFeedItems!!)
+        }
+    }
+
+    /**
      * Loads an RSS Feed from the given [url].
      *
      * @param url The string representation of url.
      */
     fun loadRSSFeed(url: String) {
+        // Reset cached Feed items on start loading.
+        mFeedItems = null
         getView()?.showLoadingProgress()
 
-        val loadRSSObservable = Single.fromCallable {
-            RSSLoader().loadRSS(url)
-        }
+        val loadRSSObservable = provideLoadRSSRxSingle(url)
 
         loadRSSObservable.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(object : SingleSubscriber<RSSFeed>() {
                     override fun onSuccess(value: RSSFeed) {
-                        getView()?.showRSSFeed(value.mFeedItems.toList())
+                        mFeedItems = value.mFeedItems
+                        getView()?.showRSSFeed(mFeedItems!!)
                     }
 
                     override fun onError(error: Throwable) {
                         getView()?.showError()
                     }
                 })
+    }
+
+    private fun provideLoadRSSRxSingle(url: String): Single<RSSFeed> {
+        return Single.fromCallable {
+            RSSLoader().loadRSS(url)
+        }
     }
 }
