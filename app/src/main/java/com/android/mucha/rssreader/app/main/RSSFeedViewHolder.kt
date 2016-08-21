22f65ced3bc7a794ad0b94002e26054bd596937e
Patrik Mucha <pat.mucha@gmail.com>
2016-08-21 18:03:19 +0200

    + Add basic CardView Feed layout.

--- app/src/main/java/com/android/mucha/rssreader/app/main/RSSFeedViewHolder.kt
+++ app/src/main/java/com/android/mucha/rssreader/app/main/RSSFeedViewHolder.kt
@@ -2,6 +2,7 @@ package com.android.mucha.rssreader.app.main
 
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.text.Html
 import android.view.View
 import com.android.mucha.rssreader.rssloading.RSSFeedItem
 import kotlinx.android.synthetic.main.list_item_rss_feed.view.*
@@ -12,14 +13,14 @@ import java.text.DateFormat
  *
  * @author Patrik Mucha
  */
-class RSSFeedViewHolder(val mItemView: View) : RecyclerView.ViewHolder(mItemView) {
+class RSSFeedViewHolder(private val mItemView: View) : RecyclerView.ViewHolder(mItemView) {
 
     /**
      * Reads the content of given [RSSFeedItem] and sets up all the views.
      */
     fun setRSSFeedItem(item: RSSFeedItem) {
         mItemView.feed_item_title.text = item.mTitle
-        mItemView.feed_item_description.text = item.mDescription
+        mItemView.feed_item_description.text = Html.fromHtml(item.mDescription)
         mItemView.feed_item_pubdate.text = DateFormat.getInstance().format(item.mPubDate)
         mItemView.setOnClickListener { view ->
             if (item.mLink != null) {
